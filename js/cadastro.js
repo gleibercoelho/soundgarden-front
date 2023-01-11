b3fa245c9c47df5ea229b86219d5b9aca67edef1
GFC <gleiberfelipe@hotmail.com>
2023-01-11 20:50:02 -0300

    Pagina de Js

---
+++ js/cadastro.js
@@ -0,0 +1,84 @@
+const novoEvento = async () => {
+const nomeEvento = document.getElementById('nome');
+const atracoesEvento = document.getElementById('atracoes');
+const descricaoEvento = document.getElementById('descricao');
+const dataEvento = document.getElementById('data');
+const lotacaoEvento = document.getElementById('lotacao');
+/* const formularioCadastro = document.getElementById('formulario'); */
+
+var raw = {
+    "name": nomeEvento.value,
+    "attractions": [atracoesEvento.value],
+    "description": descricaoEvento.value,
+    "scheduled": dataEvento.value,
+    "number_tickets": lotacaoEvento.value,
+    
+    
+};
+
+var requestOptions = {
+    method: 'POST',
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(raw),
+    /* redirect: 'follow' */
+};
+
+
+const postarDados = await fetch("https://soundgarden-api.deta.dev/events", requestOptions)
+.then(response => response.text())
+.then(result => console.log(result))
+.catch(error => console.log('error', error));
+
+ 
+
+};
+
+
+const botaoCadastrar = document.getElementById('btnCadastro');
+botaoCadastrar.onclick = novoEvento;
+
+
+
+
+
+
+/*   botaoCadastrar.addEventListener('submit', function () {
+    event.preventDefault();
+    console.log(nomeEvento.value, atracoesEvento.value, descricaoEvento.value, dataEvento.value, lotacaoEvento.value);
+});
+ */
+
+/* formCEP.addEventListener("submit", async (form) => {
+    form.preventDefault();
+  
+    feedbackBuscando();
+  
+    const resposta = await buscaCep(inputCEP.value);
+  
+    if (resposta?.message) {
+      alert(resposta?.message);
+    } else {
+      estado.value = resposta?.state;
+      cidade.value = resposta?.city;
+      bairro.value = resposta?.neighborhood;
+      rua.value = resposta?.street;
+      
+    }
+  
+    feedbackBuscando(false);
+  }); */
+
+/* {
+  "name": "Evento teste 2",
+  "poster": "link da imagem",
+  "attractions": [
+    "Cantor 1"
+  ],
+  "description": "Evento incrivel",
+  "scheduled": "2022-03-24T00:57:37.761Z",
+  "number_tickets": 10,
+  "_id": "623e7e7de6e2db10816075c0",
+  "created_at": "2022-03-26T02:46:21.918Z",
+  "updated_at": "2022-03-26T02:46:21.918Z",
+  "__v": 0
+} */
