7495d2612a73113fc455e9cfbd434290962d6d15
GFC <gleiberfelipe@hotmail.com>
2023-01-15 14:35:14 -0300

    stuck 2

--- js/eventos.js
+++ js/eventos.js
@@ -43,35 +43,34 @@ const novoEvento = async () => {
     const ownerBooking = document.getElementById('ownerImput');
     const EmailBooking = document.getElementById('EmailImput');
     const IdBooking = document.getElementById('IdImput');
-    const iddoevento = document.getElementById('h5')
-
+    const number_ticketsValue = 1;
 
 
     var raw = {
         "owner_name": ownerBooking.value,
         "owner_email": EmailBooking.value,
-        "number_tickets": 1,
-        "event_id": iddoevento.value
+        "number_tickets": number_ticketsValue,
+        "event_id": IdBooking.value
     };
 
 
     var requestOptions = {
         method: 'POST',
-        body: raw,
-        redirect: 'follow',
-        headers: { "Content-Type": "application/json" }
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(raw),
+        /* redirect: 'follow', */
+        
     };
     const postarDados = await fetch("https://soundgarden-api.deta.dev/bookings", requestOptions)
         .then(response => response.text())
         .then(result => console.log(result))
-        .catch(error => console.log('error', error));
-};
+        .catch(error => console.log('error', error));};
 
 
-const formBooking = document.getElementById('vis-modal');
+const formBooking = document.getElementById('formBooking');
 
 formBooking.addEventListener('submit', function (event) {
     event.preventDefault();
     novoEvento();
-    console.log(ownerBooking.value, EmailBooking.value, IdReserva.value );
+    console.log(ownerBooking.value, EmailBooking.value, IdReserva.value);
 });
