d4a36018d9343ff7731aa168750e95a011ff9074
GFC <gleiberfelipe@hotmail.com>
2023-01-18 16:01:22 -0300

    Final Version

--- js/eventos.js
+++ js/eventos.js
@@ -1,8 +1,13 @@
+const divEventos = document.getElementById('eventos');
+const criacaoModal = document.getElementById('modalDiv');
+const IdImput = document.getElementById('IdImput')
+let contador = 0;
+let contador2 = 0;
 
 
-const divEventos = document.getElementById('paginaEventos');
-const IdReserva = document.getElementById('IdEvento');
-
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+IdImput.value = id;
 
 const getEvents = async () => {
 
@@ -11,31 +16,75 @@ const getEvents = async () => {
 
     events.forEach((event) => {
         const article = document.createElement('article');
+        contador = contador + 1
         article.innerHTML = `
             <article class="evento card p-5 m-3" id="card">
-                <h2>${event.name} - ${new Date(event.scheduled).toLocaleDateString("pt-BR")}</h2>
-                <h5 id="h5"> ID ${event._id}</h5>
+                <div class="headereventos">
+                <h2>${event.name}  ${new Date(event.scheduled).toLocaleDateString("pt-BR")}</h2>
                 <h4>${event.attractions.join(", ")}</h4>
+                </div>
                 <p>${event.description}</p>
-                <a href="#" onclick="acao()" class="btn btn-primary">reservar ingresso</a>
+                <a href="#" id="${event._id}" onclick="btn('${event._id}')"  class="btn btn-primary">reservar ingresso</a>
             </article>`;
 
         divEventos.appendChild(article);
-    })
-};
 
+        var idEvento = "";
+
+    })
+
+    /* const modmod = await fetch(BASE_URL).then((result) => result.json());
+    modmod.sort((eventoA, eventoB) => eventoA.scheduled - eventoB.scheduled)
+    .slice(0 - 3)
+    .forEach((mod) =>{const divModal = document.createElement('div');
+    contador2 = contador2+1;
+    
+    divModal.innerHTML = `
+    <div class="modalbody" id="modalDiv" value="${contador2}" display="display:none;">
+        <header id="">
+         <h1>Fazer Reserva</h1>
+         </header>
+            <div>
+            <form id="formBooking">
+            <span>Nome</span>
+            <input type="text" name="Nome" id="ownerImput" placeholder="Nome Completo">
+            <span>Email</span>
+            <input type="email" name="email" id="EmailImput" placeholder="Email">
+            <span>ID</span>
+            <input type="name" value="${mod._id}" name="id" id="IdImput">
+    
+        <button type="submit" class="btn-primary">Reservar</button>
+        <button class="btn-primary" onclick="acao(evento.${mod._id})">fechar Janela</button>
+                </form>
+            </div>
+    </div>`;
+    
+    criacaoModal.appendChild(divModal);})
+    
+    
+     */
+};
 
 getEvents();
 
-function acao() {
-    let modal = document.querySelector('.modal');
-    if (modal.style.display === "block") {
-        modal.style.display = 'none';
-    } else {
-        modal.style.display = 'block';
-    }
-};
 
+function btn(idEvento) {
+    abrirmodal('ModalDiv');
+    IdImput.value = idEvento;
+    console.log(idEvento, typeof (idEvento))
+
+}
+
+function abrirmodal(carregarmodal) {
+    let modal = document.querySelector('.modal');
+    modal.style.display = 'block';
+}
+
+function fechar(fecharmodal) {
+    let modalfechar = document.querySelector('.modal');
+    modalfechar.style.display = 'none';
+
+}
 
 /***********************************************cadastro da reserva ************************************************** */
 
@@ -56,21 +105,21 @@ const novoEvento = async () => {
 
     var requestOptions = {
         method: 'POST',
-        headers: {"Content-Type": "application/json"},
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(raw),
         /* redirect: 'follow', */
-        
+
     };
     await fetch("https://soundgarden-api.deta.dev/bookings", requestOptions)
         .then(response => response.text())
         .then(result => console.log(result))
-        .catch(error => console.log('error', error));};
-
+        .catch(error => console.log('error', error));
+};
 
 const formBooking = document.getElementById('formBooking');
 
 formBooking.addEventListener('submit', function (event) {
     event.preventDefault();
     novoEvento();
-    console.log(ownerBooking.value, EmailBooking.value, IdReserva.value);
+    console.log(ownerBooking.value, EmailBooking.value, IdBooking.value);
 });
